b54d92fb4dd02e1c04d2d3f66431f58333eeb46a
kevinharv <kharvey2002@gmail.com>
2023-05-06 21:47:11 -0500

    feat: md support; removed pocketbase

--- app/blog/[post]/page.tsx
+++ app/blog/[post]/page.tsx
@@ -1,7 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { remark } from "remark"
+import html from "remark-html"
+import styles from './local.module.css'
 
+const postDirectory = path.join(process.cwd(), "md");
 
 export default async function Post({ params }: { params: {post: string}}) {
+    const fullPath: any = path.join(postDirectory, `${params.post}.md`);
+    const fileContents: any = fs.readFileSync(fullPath, 'utf8');
+
+    const processedContent = await remark().use(html).process(fileContents);
+    const parsedHTML = processedContent.toString();
+    
     return (
-        <h1>{params.post}</h1>
+        <div className='flex justify-center'>
+            <div className='max-w-3xl'>
+                <div className={styles.md} dangerouslySetInnerHTML={{ __html: parsedHTML }} />
+            </div>
+        </div>
     )
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,37 @@
+import fs from 'fs';
+import Link from 'next/link';
+import path from 'path';
 
+function getFilenamesWithoutExtension(directoryPath: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                reject(err);
+                return;
+            }
 
-export default function Blog() {
+            const filenamesWithoutExtension = files.map((file) => {
+                const extensionIndex = file.lastIndexOf('.');
+                return extensionIndex > 0 ? file.substring(0, extensionIndex) : file;
+            });
+
+            resolve(filenamesWithoutExtension);
+        });
+    });
+}
+
+export default async function Blog() {
+    const files = await getFilenamesWithoutExtension(path.join(process.cwd(), 'md'));
     return (
-        <>
-            <h1 className="text-5xl text-center">BLOG</h1>
-        </>
+        <div className='flex justify-center'>
+            <div className='flex flex-col'>
+                <h1 className="text-5xl">BLOG</h1>
+                <div className='flex justify-center'>
+                    {files?.map((file) => {
+                        return <Link key={file} href={`/blog/${file}`} className='text-4xl'>{file}</Link>
+                    })}
+                </div>
+            </div>
+        </div>
     )
 }
